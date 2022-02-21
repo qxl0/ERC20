93012fd559a28bb0bde23db53efa90ea28decfe8
Qiang <qxl0@yahoo.com>
2022-02-21 07:52:54 -0500

    Get first contract copied from openzeppelin

---
+++ contracts/OurToken.sol
@@ -0,0 +1,12 @@
+// contracts/GLDToken.sol
+// copied from : https://docs.openzeppelin.com/contracts/4.x/erc20
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract GLDToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Gold", "GLD") {
+        _mint(msg.sender, initialSupply);
+    }
+}
